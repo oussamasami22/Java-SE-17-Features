992b0801177d153032eaf84a59a75ab7a0195172
OUSSAMA ELG <oussama.elgoum@gmail.com>
2025-07-12 16:34:23 +0100

    Prdicate Interface

--- src/main/java/ma/ensa/StreamsPredicate.java
+++ src/main/java/ma/ensa/StreamsPredicate.java
@@ -1,6 +1,42 @@
 package ma.ensa;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
 public class StreamsPredicate {
 
+ static class Person{
+     String name ;
+     int age;
+     String email;
+     public Person(String name,int age , String email){
+         this.name = name;
+         this.age = age;
+         this.email = email;
+
+     }
+
+     @Override
+     public String toString(){
+         return name + "(" +age+ ") -" +email;
+     }
+
+ }
+    public static void main(String[] args) {
+
+        // Liste de personnes
+        List<Person> people = Arrays.asList(
+                new Person("Ahmed", 22, "ahmed@gmail.com"),
+                new Person("Amina", 17, "amina@gmail.com"),
+                new Person("Ali", 30, "ali@"),
+                new Person("Khalid", 25, "khalid@gmail.com"),
+                new Person("Amal", 19, "amal@gmail.com")
+        );
+        Predicate<Person> isAdult = p -> p.age >=18;
+
+    }
 
 }
+
+
