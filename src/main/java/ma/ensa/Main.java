9e16c48c863560def39a8908cf386bb2860c08b3
OUSSAMA ELG <oussama.elgoum@gmail.com>
2025-07-12 20:10:04 +0100

    update 4

--- src/main/java/ma/ensa/Main.java
+++ src/main/java/ma/ensa/Main.java
@@ -4,22 +4,34 @@ import ma.ensa.Interfaces.InterestStrategy;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 
 public class Main {
     public static void main(String[] args) {
         List<BankAccount> accounts = Arrays.asList(
-                new BankAccount("oussama ",2300),
-                new BankAccount("sami", 4000),
+                new BankAccount("oussama ",22300),
+                new BankAccount("sami", 40000),
                 new BankAccount("ayoub", 4000)
         );
         Supplier<InterestStrategy> fixedStrategySupplier  =() -> balance -> balance * 0.5;
         Predicate<BankAccount> isVIP =  b -> b.balance >=10000;
         Function<BankAccount, Double> interestCalculator =
                 acc -> fixedStrategySupplier.get().calculateInterest(acc.balance);
+        Consumer<BankAccount> printInterest = acc -> {
+            double interestOwner = interestCalculator.apply(acc);
+            System.out.println(acc.owner + "will recieve $" +interestOwner+ " as interest.");
+        };
+        System.out.println("🏦 Processing VIP Accounts:\n");
+        accounts.stream()
+                .filter(isVIP)
+                .peek(printInterest)
+                .map(acc -> acc.applyInterest(fixedStrategySupplier.get()))
+                .forEach(newBalance -> System.out.println("New Balance: $" + newBalance + "\n"));
 
     }
 
